2fac3608356db8328ebfd89a1781ae77360de9d5
yuanyulong <yuanyulong89@gmail.com>
2015-04-16 21:16:41 +0800

    2015/01/16/21

--- src/AlibabaComp.java
+++ src/AlibabaComp.java
@@ -8,18 +8,10 @@ public class AlibabaComp {
 
 	public static void main(String[] args) {
 		Data data = new Data();
-		//data.readData("D:\\Users\\yuanyulong\\workspace\\AlibabaComp\\files\\tianchi_mobile_recommend_train_user\\tianchi_mobile_recommend_train_user.csv");
-		SimpleDateFormat mydf = new SimpleDateFormat("yyyy-MM-dd");
-		try {
-			Date dt = mydf.parse("2015-03-05");
-			Calendar cal = Calendar.getInstance();
-			cal.setTime(dt);
-			System.out.println(cal.get(Calendar.DAY_OF_WEEK)-1);
-
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		long starttime = System.currentTimeMillis();
+		data.readData("D:\\Users\\yuanyulong\\workspace\\AlibabaComp\\files\\tianchi_mobile_recommend_train_user\\tianchi_mobile_recommend_train_user.csv");
+		long endtime = System.currentTimeMillis();
+		System.out.println(""+(endtime-starttime)+"ms");
 		
 	}
 
--- src/Data.java
+++ src/Data.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 public class Data {
 	public class Train_User
@@ -14,12 +16,18 @@ public class Data {
 		public Time time; 
 	}
 	HashMap<Time,Vector<Train_User>> data;//按日期划分保存的用户购买集
+	static float count=0;
+	public Data()
+	{
+		data = new HashMap<Time,Vector<Train_User>>();
+	}
 	//从文件中读取数据的函数
 	public void readData(String filename)
 	{
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(filename));
 			String s = null;
+			s=br.readLine();
 			while((s=br.readLine())!=null)
 			{
 				String[] str = s.split(",");
@@ -30,14 +38,68 @@ public class Data {
 				tu.user_geohash = str[3];
 				tu.item_category = str[4];
 				
+				String[] str_time = str[5].split(" ");
+				SimpleDateFormat mydf = new SimpleDateFormat("yyyy-MM-dd");
+				Date dt = mydf.parse(str_time[0]);
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(dt);
+				tu.time = new Time();
+				tu.time.setYear(cal.get(Calendar.YEAR));
+				tu.time.setMonth(cal.get(Calendar.MONTH));
+				tu.time.setDay(cal.get(Calendar.DAY_OF_MONTH));
+				tu.time.setHour(Integer.parseInt(str_time[1]));
+//是否是工作日				
+				if((cal.get(Calendar.DAY_OF_WEEK)-1)>=1&&(cal.get(Calendar.DAY_OF_WEEK)-1)<=5)
+				{
+					tu.time.setType(1);
+				}else//是否是周末
+				{
+					tu.time.setType(2);
+				}
+//是否是双十二这以特殊的日期
+				if(cal.get(Calendar.MONTH)==12&&cal.get(Calendar.DAY_OF_MONTH)==12)
+				{
+					tu.time.setType(3);
+				}
+				
 				//data.get(key);
-				System.out.println(s);
+				//System.out.println(s);
+				/*System.out.println(tu.user_id);
+				System.out.println(tu.item_id);
+				System.out.println(tu.behavior_type);
+				System.out.println(tu.user_geohash);
+				System.out.println(tu.item_category);
+				System.out.println(tu.time.getYear());
+				System.out.println(tu.time.getMonth());
+				System.out.println(tu.time.getDay());
+				System.out.println(tu.time.getType());
+				System.out.println("============");*/
+//生成时间的键值
+				Time key_time = new Time();
+				key_time.setYear(tu.time.getYear());
+				key_time.setMonth(tu.time.getMonth());
+				key_time.setDay(tu.time.getDay());
+				key_time.setHour(-1);
+				key_time.setType(tu.time.getType());
+				if(data.get(key_time)==null)
+				{
+					Vector<Train_User> vec_value_tu = new Vector<Train_User>();
+					vec_value_tu.add(tu);
+					data.put(key_time, vec_value_tu);
+				}else
+				{
+					data.get(key_time).add(tu);
+				}
+
 			}
 			br.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
